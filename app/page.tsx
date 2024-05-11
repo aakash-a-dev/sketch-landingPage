57cc45cdbb56418394fe99e162d337d917d10883
Aakash <126223325+aakash-a-dev@users.noreply.github.com>
2024-05-12 01:40:00 +0530

    About us Section Layout done

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,16 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Navbar from "./(landing-page)/Navbar";
 import Hero from "./(landing-page)/Hero";
+import About from "./(landing-page)/About";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <Hero/>
+      <Hero />
+      <div className="flex justify-center items-center">
+        <About />
+        </div>
     </>
   );
 }
