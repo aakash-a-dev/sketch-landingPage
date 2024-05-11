124594c784a0843c9c4a35528fae8e1bb87f9a76
Aakash <126223325+aakash-a-dev@users.noreply.github.com>
2024-05-12 01:27:46 +0530

    Hero Section done

---
+++ app/(landing-page)/Hero.tsx
@@ -0,0 +1,35 @@
+import { SparklesCore } from '@/components/ui/sparkles'
+import React from 'react'
+
+function Hero() {
+  return (
+     <div className="h-[40rem] w-full flex flex-col items-center justify-center overflow-hidden rounded-md">
+      <h1 className="md:text-lg text-lg lg:text-9xl font-bold text-center text-primary relative z-20">
+        Welcome To My Art Universe
+      </h1>
+      <div className="w-[40rem] h-40 relative">
+        {/* Gradients */}
+        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-primary to-transparent h-[2px] w-3/4 blur-sm" />
+        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-primary to-transparent h-px w-3/4" />
+        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-primary to-transparent h-[5px] w-1/4 blur-sm" />
+        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-primary to-transparent h-px w-1/4" />
+ 
+        {/* Core component */}
+        <SparklesCore
+          background="transparent"
+          minSize={0.4}
+          maxSize={1}
+          particleDensity={1200}
+          className="w-full h-full"
+          particleColor="#000"
+        />
+ 
+        {/* Radial Gradient to prevent sharp edges */}
+        <div className="absolute inset-0 w-full h-full bg-white [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+      </div>
+    </div>
+
+  )
+}
+
+export default Hero
